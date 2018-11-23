a7a4169b4e0bd174a5de81dfafbde36afe993ccd
Pedram Tavadze <petavazohi@mix.wvu.edu>
2018-11-23 16:52:34 -0500

    replaced the vacuum addin part with the already build in function in pychemia

--- wf_poscar_prep.py
+++ wf_poscar_prep.py
@@ -1,13 +1,14 @@
 """
-This script is meant to search in the database 
-for different substrates for FeS
+This script will prepare POSCAR for work-function calculation
+it will generate different terminations with the amount of 
+vacuum specified
 Author : Pedram Tavadze
 Email  : petavazohi@mix.wvu.edu
 """
-import json
+
 import numpy as np
 import pychemia
-from pymatgen.io import vasp
+
 
 def remove_atom(st,indices):
     removed_struct = pychemia.core.Structure(symbols=list(np.delete(st.symbols,indices)),cell=st.cell,
@@ -34,56 +35,28 @@ def add_layers(structure,nlayer,length,direction):
     #new_structure = pychemia.core.Structure(symbols=new_structure.symbols, cell=newlattice.cell, positions=new_structure.positions)
     new_st_added_layers.sort_sites()
     n_unique_layers = len(np.unique(st.positions[:,direction].round(decimals=2)))
-#    vacuum = np.zeros(3)
-#    vacuum[direction] = length
-#    alpha = st.lattice.alpha
-#    beta = st.lattice.beta
-#    gamma = st.lattice.gamma
-#    newlenghts = st.lattice.lengths + vacuum
-#    a = newlenghts[0]
-#    b = newlenghts[1]
-#    c = newlenghts[2]
-#    newlattice = st.lattice.from_parameters_to_cell(a, b, c, alpha, beta, gamma)
     structures = []
 
     for i in range(1,n_unique_layers+1):
         temp_st = new_st_added_layers.copy()
         item = np.unique(temp_st.positions[:,direction])[i*-1]
-        layer_index = temp_st.positions[:,direction] >= item
+        layer_index = temp_st.positions[:,direction].round(decimals=2) >= item
         indices = []
         for iatom in range(temp_st.natom):
             if layer_index[iatom] :
                 indices.append(iatom)
-
-
         temp_st = remove_atom(temp_st,indices=indices)
         a = temp_st.lattice.lengths[0]
         b = temp_st.lattice.lengths[1]
         c = max(temp_st.positions[:,direction][temp_st.positions[:,direction] < item])
         newlattice = st.lattice.from_parameters_to_cell(a, b, c, alpha, beta, gamma)
         new_structure = pychemia.core.Structure(symbols=temp_st.symbols, cell=newlattice.cell, positions=temp_st.positions)
-
+        new_structure = new_structure.add_vacuum(length,direction)
         structures.append(new_structure)
     return structures
 
 
 
-#rf = open("129-2_FeTe_000000000000000111166.json")
-#data = json.load(rf)
-#rf.close()
-#
-#formula = [x for x in data][0]
-#_id = data[formula]['_id']
-#
-#dbsettings={'host'  : 'mongo01.systems.wvu.edu', 
-#            'name'  : 'PyChemiaMasterDB', 
-#            'user'  : 'guest', 
-#            'passwd': 'aldo', 
-#            'ssl'   : True}
-#pcdb=pychemia.db.get_database(dbsettings)
-#
-#st = pcdb.get_structure(_id)
-#pychemia.code.vasp.write_poscar(st,"POSCAR")
 st = pychemia.code.vasp.read_poscar("POSCAR")
 cs = pychemia.crystal.CrystalSymmetry(st)
 st = cs.refine_cell()
